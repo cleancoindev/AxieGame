63b2f791f6f52550a216a27eec34a1ee662f314f
cesarsld <jaimes.cesar88@gmail.com>
2018-09-24 22:56:30 +0100

    Add project files.

---
+++ Axie/server.js
@@ -0,0 +1,13 @@
+'use strict';
+console.log(__dirname);
+var http = require('http');
+var port = process.env.PORT || 1337;
+var express = require('express');
+var app = express();
+app.use('/assets' ,express.static('public'));
+var serv = require('http').Server(app);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+serv.listen(1337);
+console.log("Server started.");
