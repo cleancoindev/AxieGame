71b1961c484d3c8e9d17a8616537b10c5e64a53f
cesarsld <jaimes.cesar88@gmail.com>
2018-10-09 23:46:21 +0100

    Started implementing game rooms

--- Axie/server.js
+++ Axie/server.js
@@ -7,10 +7,14 @@ var path = require('path');
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
+//var react = require('react')(server);
 var port = process.env.PORT || 1337;
 const AXIE_SPEED = 10;
 
 var axies = {};
+var entryPool = {}; 
+var gameRooms = {};
+var roomIndex = 1;
 //app.use(function (req, res, next) {
     //res.setHeader('Access-Control-Allow-Origin', 'http://http://86.143.229.152:1337');
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1337/');
@@ -38,11 +42,7 @@ io.on('connection', function(socket){
         id : socket.id, 
         coordinates : axies[socket.id]
     });
-    socket.emit('News', 'Hello world!');
     console.log('user connected');
-    socket.on('echo1', function(data){
-        console.log(data);
-    });
     socket.on('updateAxieMovement', function(movement){
         //console.log('received data');
         if (movement.up) axies[socket.id].y -= AXIE_SPEED;
@@ -51,12 +51,34 @@ io.on('connection', function(socket){
         if (movement.right) axies[socket.id].x += AXIE_SPEED;
         io.sockets.emit('updatedAxieMovement', axies);
     });
+
+    socket.on('createGameRoom', function () {
+        gameRooms[roomIndex++] = createGameRoom();
+        gameRooms[id].players[socket.id] = axie[socket.id];
+    }
+    });
+    socket.on('joinGameRoom', function (id) {
+        if (gameRooms[id].players < 4) {
+            socket.emit('loadOtherPlayers', gameRooms[id].players);
+            gameRooms[id].players[socket.id] = axie[socket.id];
+            
+        }
+        else //send cant join
+    });
+
     socket.on('disconnect', function () {
         delete axies[socket.id];
         socket.broadcast.emit('userDisconnected', socket.id);
     });
 });
 
+
+function createGameRoom() {
+    //this.id = _id;
+    this.players = {};
+    this.acceptingPlayers = true;
+    return this;
+}
 
 server.listen(1337, function(){
     console.log("Server started. Listening on port 1337.");
